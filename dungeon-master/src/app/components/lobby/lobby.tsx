1f519350ac45e8269158b6c404477b632c34cf36
TierraCodes <tdwilliams2004@gmail.com>
2025-09-25 16:46:18 -0400

    updated game lobby to include multiple sessions

--- dungeon-master/src/app/components/lobby/lobby.tsx
+++ dungeon-master/src/app/components/lobby/lobby.tsx
@@ -5,17 +5,31 @@ export default function Home() {
 const router = useRouter()
 
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div>
       <header>
         <h1>Welcome to the Dungeon</h1>
 
       </header>
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <main>
         <div>
           <em>What action will you take?</em>
           <div className="start-buttons-container">
-            <button className="start-buttons" type='button' onClick={()=>router.push('/components/game')}>Join Game</button>
+
             <button className="start-buttons" type='button' onClick={()=>router.push('/sessionHistory')}>Reminisce</button>
+
+            <div id="game-sessions-div">
+              <div id="game-sessions-left-div">
+                <button className="game-session-buttons" type='button' onClick={()=>router.push('/components/game')}>Join Game Session</button>
+                <button className="game-session-buttons" type='button' onClick={()=>router.push('/components/game')}>Join Game Session</button>
+                <button className="game-session-buttons" type='button' onClick={()=>router.push('/components/game')}>Join Game Session</button>
+              </div>
+              <div id="game-sessions-right-div">
+                <button className="game-session-buttons" type='button' onClick={()=>router.push('/components/game')}>Join Game Session</button>
+                <button className="game-session-buttons" type='button' onClick={()=>router.push('/components/game')}>Join Game Session</button>
+                <button className="game-session-buttons" type='button' onClick={()=>router.push('/components/game')}>Join Game Session</button>
+              </div>
+            </div>
+
           </div>
         </div>
 
