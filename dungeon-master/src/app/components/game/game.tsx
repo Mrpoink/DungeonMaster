481c092c61c20b20c198db83a1659dcd3853314c
TierraCodes <tdwilliams2004@gmail.com>
2025-09-29 14:41:13 -0400

    Updated game buttons and other styling

--- dungeon-master/src/app/components/game/game.tsx
+++ dungeon-master/src/app/components/game/game.tsx
@@ -1,17 +1,29 @@
 'use client'
 import { useRouter } from "next/navigation";
+import Image from "../assets/mainBackground.png";
 
 export default function Game() {
 const router = useRouter()
 
   return (
-    <div className="game">
+    <div>
         <header>
-            <h1>Welcome Player</h1>
+            <h1>Dungeon Master:</h1>
+            <div>
+              <blockquote>
+                You enter into combat.<br></br> Roll for initiative.
+              </blockquote>
+            </div>
         </header>
-        <body>
-          
-        </body>
+        <main className="game">
+          <img src={Image.src} alt="" />
+          <div className="player-actions">
+            <input type="text" placeholder="What do you do?" />
+            <button className="button-33">
+              ROLL
+            </button>
+          </div>
+        </main>
     </div>
   )
 }
