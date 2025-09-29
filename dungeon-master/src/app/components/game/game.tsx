f07b075e01870405d9d48c84464b92fb618a887d
TierraCodes <tdwilliams2004@gmail.com>
2025-09-29 18:20:46 -0400

    Added dice options and roll functionality

--- dungeon-master/src/app/components/game/game.tsx
+++ dungeon-master/src/app/components/game/game.tsx
@@ -1,9 +1,46 @@
 'use client'
 import { useRouter } from "next/navigation";
-import Image from "../assets/mainBackground.png";
+import { MouseEventHandler, useState } from "react";
+import Background from "../assets/mainBackground.png";
+import D20 from "../assets/d20.png";
+import D12 from "../assets/d12.png";
+import D10 from "../assets/d10.png";
+import D8 from "../assets/d8.png";
+import D6 from "../assets/d6.png";
+import D4 from "../assets/d4.png";
+
 
 export default function Game() {
-const router = useRouter()
+  const router = useRouter()
+  const [dice, setDice] = useState("d20");
+  const handleRoll = () => {
+    let sides = 20;
+    switch (dice) {
+      case "d20":
+        sides = 20;
+        break;
+      case "d12":
+        sides = 12;
+        break;
+      case "d10":
+        sides = 10;
+        break;
+      case "d8":
+        sides = 8;
+        break;
+      case "d6":
+        sides = 6;
+        break;
+      case "d4":
+        sides = 4;
+        break;
+      default:
+        sides = 20;
+    }
+    const roll = Math.floor(Math.random() * sides) + 1;
+    return roll;
+  }
+
 
   return (
     <div>
@@ -15,11 +52,20 @@ const router = useRouter()
               </blockquote>
             </div>
         </header>
+        <div className="dice">
+          <img src={D20.src} alt="" onClick={() => setDice("d20")}/>
+          <img src={D12.src} alt="" onClick={() => setDice("d12")}/>
+          <img src={D10.src} alt="" onClick={() => setDice("d10")}/>
+          <img src={D8.src} alt="" onClick={() => setDice("d8")}/>
+          <img src={D6.src} alt="" onClick={() => setDice("d6")}/>
+          <img src={D4.src} alt="" onClick={() => setDice("d4")}/>
+
+        </div>
         <main className="game">
-          <img src={Image.src} alt="" />
+          <img src={Background.src} alt="" />
           <div className="player-actions">
-            <input type="text" placeholder="What do you do?" />
-            <button className="button-33">
+            <input type="text" placeholder="What do you do?" disabled/>
+            <button onClick={handleRoll}>
               ROLL
             </button>
           </div>
