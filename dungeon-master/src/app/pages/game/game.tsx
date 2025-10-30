44a7d6889922142c4dfe4c3acaff7ef97ace8691
TierraCodes <tdwilliams2004@gmail.com>
2025-10-30 13:05:34 -0400

    attempting to refactor gameMaster component

--- dungeon-master/src/app/pages/game/game.tsx
+++ dungeon-master/src/app/pages/game/game.tsx
@@ -5,15 +5,16 @@ import Dice from "@/app/components/dice/dice";
 import GameMasterHeader from "@/app/components/gameMasterHeader/gameMasterHeader";
 import { Roll } from "@/app/components/dice/dice";
 import Party from "@/app/components/party/party";
+import GameMaster from "@/app/components/gameMasterHeader/gameMaster";
 
 
 export default function Game() {
   var turn_num = 0;
-  type ConversationMessage = { sender: 'User' | 'DM' | string; text: string };
+  // type ConversationMessage = { sender: 'User' | 'DM' | string; text: string };
   const [sides, setSides] = useState<number>(20);
   const [activeDice, setActiveDice] = useState("d20");
-  const [DMmessage, setDMmessage] = useState("Connecting...");
-  const [conversation, setConversation] = useState<ConversationMessage[]>([]);
+  // const [DMmessage, setDMmessage] = useState("Connecting...");
+  // const [conversation, setConversation] = useState<ConversationMessage[]>([]);
 
   const handleDiceSelect = (selectedSides: number) => {
     setSides(selectedSides);
@@ -23,52 +24,52 @@ export default function Game() {
   }
   
 
-  const sendUserin = async () => {
-    const userMessage = userin;
-    setConversation(prev => [...prev, {sender: 'User', text:userMessage}]);
-    send_userin('');
-    try {
-      const response = await fetch('http://localhost:1068/userin', {
-        method : 'POST',
-        headers: {
-          'Content-Type' : 'application/json',
-        },
-        body : JSON.stringify({command : userMessage}),
-      });
-      send_userin('');
+  // const sendUserin = async () => {
+  //   const userMessage = userin;
+  //   setConversation(prev => [...prev, {sender: 'User', text:userMessage}]);
+  //   send_userin('');
+  //   try {
+  //     const response = await fetch('http://localhost:1068/userin', {
+  //       method : 'POST',
+  //       headers: {
+  //         'Content-Type' : 'application/json',
+  //       },
+  //       body : JSON.stringify({command : userMessage}),
+  //     });
+  //     send_userin('');
 
-      const result = await response.json();
+  //     const result = await response.json();
 
-      setConversation(prev => [...prev, {sender : 'DM', text : result.message}]);
-    } catch (error){
-      console.error("Failed to send userin to python: ", error)
-      setConversation(prev => [...prev, { sender: 'DM', text: "Server Error: Could not connect to Python backend." }]);
-    }
+  //     setConversation(prev => [...prev, {sender : 'DM', text : result.message}]);
+  //   } catch (error){
+  //     console.error("Failed to send userin to python: ", error)
+  //     setConversation(prev => [...prev, { sender: 'DM', text: "Server Error: Could not connect to Python backend." }]);
+  //   }
     
-  };
-  const [userin, send_userin] = useState('');
+  // };
+  // const [userin, send_userin] = useState('');
 
-  const handleUserin = (event: { target: { value: SetStateAction<string> } }) => {
-    send_userin(event.target.value);
-  };
+  // const handleUserin = (event: { target: { value: SetStateAction<string> } }) => {
+  //   send_userin(event.target.value);
+  // };
 
-  const fetchDMmessage = async () => {
-    try{
+  // const fetchDMmessage = async () => {
+  //   try{
 
-      const response = await fetch('http://localhost:1068/DMout');
-      const data = await response.json();
+  //     const response = await fetch('http://localhost:1068/DMout');
+  //     const data = await response.json();
 
-      setDMmessage(data.dm_text);
-    } catch (error) {
-      console.error("Something went wrong with fetch dm message, line 81", error);
-      setDMmessage("Error: could not fetch python response, something went wrong, line 82");
-    }
-    turn_num += 1;
-  };
+  //     setDMmessage(data.dm_text);
+  //   } catch (error) {
+  //     console.error("Something went wrong with fetch dm message, line 81", error);
+  //     setDMmessage("Error: could not fetch python response, something went wrong, line 82");
+  //   }
+  //   turn_num += 1;
+  // };
 
-  useEffect(() => {
-    fetchDMmessage();
-  }, []);
+  // useEffect(() => {
+  //   fetchDMmessage();
+  // }, []);
 
   return (
     <div>
@@ -85,13 +86,13 @@ export default function Game() {
           </div>
           <main className="game">
             <img src={Background.src} alt="" />
-            <div className = "message-log">
+            {/* <div className = "message-log">
               {conversation.map((msg, index) => (
                 <p key={index} className={`message ${msg.sender.toLowerCase()}`}>
                   <strong>{msg.sender}:</strong> {msg.text}
                 </p>
               ))}
-            </div>
+            </div> */}
             <div className="player-actions">
               <input type="text" value={userin} 
                 onChange={handleUserin} placeholder="What do you do?"
