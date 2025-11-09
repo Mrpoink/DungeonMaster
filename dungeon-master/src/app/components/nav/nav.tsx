8269cbe3930510b9f14d0420ae0b130833da84db
TierraCodes <tdwilliams2004@gmail.com>
2025-11-09 16:19:13 -0500

    updated styling

--- dungeon-master/src/app/components/nav/nav.tsx
+++ dungeon-master/src/app/components/nav/nav.tsx
@@ -2,12 +2,16 @@
 import { CgProfile } from "react-icons/cg"
 import { useRouter } from "next/navigation";
 import { GrBook } from "react-icons/gr";
+import { SiDungeonsanddragons } from "react-icons/si";
 
 
 export default function Nav(){
     const router = useRouter()
     return(
       <nav className="navbar">
+        <div>
+          <SiDungeonsanddragons className="dnd-icon"/>
+        </div>
         <h1>Welcome to QuestWeaver</h1>
         <div className="character-info-div">
           <button onClick={()=>router.push('./characterInfo')}>
