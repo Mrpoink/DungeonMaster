60238a47fc831607009fbbfc6c2d0b7b0c5363e1
mrpoink <brandondean1068@gmail.com>
2025-11-09 14:37:44 -0500

    fixed event loop closed errors, then quantized model

--- PythonBackEnd.py
+++ PythonBackEnd.py
@@ -38,9 +38,9 @@ def remove_parts(initial : str):
 
 
 bnb_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_quant_type="nf4",
-    bnb_4bit_compute_dtype=torch.bfloat16
+    load_in_8bit=True,
+    bnb_8bit_quant_type="nf8",
+    bnb_8bit_compute_dtype=torch.bfloat16
 )
 
 lora_config = LoraConfig(
@@ -60,7 +60,7 @@ def def_model():
         dtype = torch.bfloat16,
         device_map = "cuda"
         )
-    model = PeftModel.from_pretrained(base_model, "prompt_classifier_Smol/checkpoint-2673")
+    model = PeftModel.from_pretrained(base_model, "prompt_classifier_Smol/checkpoint-3560", quantization_config=bnb_config)
 
     model.to("cuda")
 
@@ -115,8 +115,8 @@ async def model_output_check(userin : str, model, tokenizer): #Running check scr
             return_dict_in_generate=True,
             output_scores=True,
             do_sample=True,
-            top_p = .65,
-            #top_k = 40,
+            #top_p = .65,
+            top_k = 20,
             # pad_token_id=tokenizer.eos_token_id,
             # eos_token_id=tokenizer.eos_token_id,
             repetition_penalty = 3.3,
@@ -125,7 +125,10 @@ async def model_output_check(userin : str, model, tokenizer): #Running check scr
     decoded_ouput = tokenizer.decode(outputs[0][0], skip_special_tokens = True)
     print(decoded_ouput)
     final_output = decoded_ouput.split('[/GENERATED CHECK]:')
-    final_output = final_output[1].split('|end|')
+    try:
+        final_output = final_output[1].split('[/CHECK]')
+    except Exception:
+        final_output = final_output[1].split('|end|')
     print(len(final_output))
     
     torch.cuda.empty_cache()
@@ -191,7 +194,12 @@ class DungeonMaster:
         
     @classmethod
     async def create_dm(cls):
-        
+        try:
+            loop = asyncio.get_running_loop()
+        except RuntimeError:
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+            
         instance = cls()
 
         instance.model, instance.tokenizer = def_model()
@@ -219,6 +227,12 @@ class DungeonMaster:
     
     @classmethod
     async def create_backend(cls):
+        try:
+            loop = asyncio.get_running_loop()
+        except RuntimeError:
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+            
         instance = cls()
 
         instance.bvb = vector_database.use_vector_db()
@@ -237,7 +251,7 @@ class DungeonMaster:
             instance.scene_num += 1
             instance.scene = await instance.vb.find_scene(instance.scene_num)
 
-        prompt = f"{instance.scene} {instance.player} [/ACTION]: {instance.player_says}"
+        prompt = f"{instance.scene} {instance.player} [/ACTION]: {userin}"
 
         final_input = await final_prompt(prompt, instance.vb, instance.seed, instance.turn_num)
 
