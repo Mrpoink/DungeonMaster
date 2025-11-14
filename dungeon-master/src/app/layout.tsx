11dfe5b2ffafbd5e4273d3579bdb2bf89e5901ea
mrpoink <brandondean1068@gmail.com>
2025-11-14 12:50:01 -0500

    Revert "Optimizing for mobile clients"
    This reverts commit ec9920c7d7d46384f3aad46d53a2380c6bf04a09.
    
    this broke ebverything for desktop users

--- dungeon-master/src/app/layout.tsx
+++ dungeon-master/src/app/layout.tsx
@@ -20,7 +20,6 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "QuestWeaver",
   description: "QuestWeaver - Your D&D Adventure Awaits!",
-  viewport: "width=device-width, initial-scale=1, maximum-scale=5",
 };
 
 
